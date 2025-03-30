bfce1b8c1ab852c41f283639316a7d6baf548343
João Gabriel Gonçalves Oliveira <joaoggolive@gmail.com>
2025-03-30 18:28:12 -0300

    Add files via upload

---
+++ exerciciosJS/exercicio2/index.js
@@ -0,0 +1,12 @@
+let data = new Date();
+        let horario = data.getHours();
+        var img = window.document.getElementById('div1').innerHTML = "agora são: " + horario;
+        horario = 19;
+
+        if(horario < 12 && horaio > 0){
+
+
+        }else{
+            document.body.style.background = 'black';
+
+        }
